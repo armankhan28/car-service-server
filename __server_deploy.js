59b018d613cd81545c905fe0f817456a17deb75f
captainkhan28 <86075671+captainkhan28@users.noreply.github.com>
2022-05-07 22:54:17 +0600

    commit

---
+++ __server_deploy.js
@@ -0,0 +1,12 @@
+/*
+ --------------------------
+ One Time For Your Computer
+ --------------------------
+ 1 create heroku account
+ 2 verify email
+ 3 install heroku CLI
+ 4 heroku login
+ --------------------------
+ For each project one time
+ 1 heroku create
+*/
